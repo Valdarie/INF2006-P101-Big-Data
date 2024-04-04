56def320a18b11d3e8667df96977f1214bd84a95
Valerie <aeginana@gmail.com>
2024-04-04 12:12:01 +0800

    Revert "0.4 Task 2 III"
    This reverts commit c89440c23e8d5ffc5cbf1ac979e9c8c177d94a0a.

---
+++ AirlineSentimProject/src/main/java/task2/AirlineNegativeSentiments.java
@@ -0,0 +1,61 @@
+package task2;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.chain.ChainMapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class AirlineNegativeSentiments {
+	
+	public static void main(String[] args)
+			throws IOException, URISyntaxException, ClassNotFoundException, InterruptedException {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		
+		Job job = Job.getInstance(conf, "Airline Negative Sentiments");
+		job.setJarByClass(AirlineNegativeSentiments.class);
+		Path inPath = new Path(otherArgs[0]);
+		Path outPath = new Path(otherArgs[1]);
+		outPath.getFileSystem(conf).delete(outPath, true);
+		
+		// Put this file to distributed cache so we can use it to join
+		job.addCacheFile(new URI(otherArgs[2]));
+		
+		Configuration validationConf = new Configuration(false);
+		ChainMapper.addMapper(job, ANSValidationMapper.class, LongWritable.class, Text.class, LongWritable.class, 
+					Text.class, validationConf);
+		
+		Configuration ansConf = new Configuration(false);
+		ChainMapper.addMapper(job, ANSMapper.class, LongWritable.class, Text.class,Text.class, IntWritable.class,
+				ansConf);
+		
+		job.setMapperClass(ChainMapper.class);
+		// job.setCombinerClass(ANSReducer.class);
+		job.setReducerClass(ANSReducer.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		FileInputFormat.addInputPath(job, inPath);
+		FileOutputFormat.setOutputPath(job, outPath);
+		boolean status = job.waitForCompletion(true);
+		
+		if (status) {
+			System.exit(0);
+		} 
+		else {
+			System.exit(1);
+		}
+	}
+}
+	
