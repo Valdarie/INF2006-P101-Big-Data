43e57fd1dcb757f8d76b8d14d8c56153b6a0076b
Valerie <aeginana@gmail.com>
2024-04-07 02:35:18 +0800

    1.1 Task3 Country Reducer

--- AirlineSentimProject/src/main/java/task3/CountryReducer.java
+++ AirlineSentimProject/src/main/java/task3/CountryReducer.java
@@ -5,14 +5,44 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class CountryReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    private Map<Text, IntWritable> countryCountMap = new HashMap<>();
+    private Text mostComplainedCountry = new Text();
+    private IntWritable maxComplaints = new IntWritable(0);
+
     @Override
     protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
         int sum = 0;
         for (IntWritable val : values) {
             sum += val.get();
         }
-        context.write(key, new IntWritable(sum));
+        Text country = new Text(key);
+        IntWritable count = new IntWritable(sum);
+        countryCountMap.put(country, count);
+
+        // Check if this country has the most complaints
+        if (sum > maxComplaints.get()) {
+            mostComplainedCountry.set(country);
+            maxComplaints.set(sum);
+        }
+    }
+
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+        // Remove the most complained country from the map
+        countryCountMap.remove(mostComplainedCountry);
+
+        // Write the country with the most complaints at the top
+        context.write(new Text("Most complained country: " + mostComplainedCountry.toString()), maxComplaints);
+
+        // Sort the remaining countries alphabetically and write them
+        Map<Text, IntWritable> sortedMap = new TreeMap<>(countryCountMap);
+        for (Map.Entry<Text, IntWritable> entry : sortedMap.entrySet()) {
+            context.write(entry.getKey(), entry.getValue());
+        }
     }
 }
